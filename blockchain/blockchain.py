011152487c3d23da23bc79afd180a81a609d4c10
Gerardo Galan <g.galan@ukko.mx>
2020-08-29 18:41:33 -0500

    Add server

--- blockchain/blockchain.py
+++ blockchain/blockchain.py
@@ -2,16 +2,34 @@
 from block import Block
 import datetime
 
-num_blocks_to_add = 10
+class Blockchain:
+    def __init__():
+        block_chain = [Block.create_genesis_block()]
+        print("El bloque genesis ha sido creado.")
+        print("Hash: %s" % block_chain[0].hash)
+        i = 1
 
-block_chain = [Block.create_genesis_block()]
+    def write_block(i):
+        datos = str("Numero de bloque %d \n" % i)
+        datos += input("Dame un contrato: \n")
+        block_chain.append(Block(block_chain[i-1].hash,
+                                 datos,
+                                 datetime.datetime.now()))
 
-print("The genesis block has been created.")
-print("Hash: %s" % block_chain[0].hash)
+        print("Bloque #%d creado." % i)
+        print("Hash: %s" % block_chain[-1].hash)
+        if input("¿Quieres agregar otro bloque? [s/n]\n") in ["N","n"]:
+            return 0
+        return i+1
 
-for i in range(1, num_blocks_to_add):
-    block_chain.append(Block(block_chain[i-1].hash,
-                             "Block number %d" % i,#aqui irian los datos
-                             datetime.datetime.now()))
-    print("Block #%d created." % i)
-    print("Hash: %s" % block_chain[-1].hash)
+
+
+    def read_block(i):
+        try:
+            block_number = int(input("dame el numero del bloque que quieres buscar:\n"))
+            print("\n" + block_chain[block_number].data + "\n")
+        except:
+            print("Ese bloque no existe")
+        finally:
+            if input("¿Quieres algun otro bloque? [s/n]\n") in ["N","n"]:
+                return
---
+++ blockchain/task_queue.py
@@ -0,0 +1,19 @@
+import threading
+import time
+
+start = time.perf_counter()
+
+def do_something():
+    print('Sleeping 1 second...')
+    time.sleep(1)
+    print('Done Sleeping...')
+
+t1 = threading.Thread(target=do_something)
+t2 = threading.Thread(target=do_something)
+
+t1.start()
+t2.start()
+
+finish = time.perf_counter()
+
+print(f'Finishes in {round(finish-start, 2)} seconds(s)')
